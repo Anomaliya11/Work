590aefd86aebdab07c7314d442a2c8326aff97c3
Katherine Swanson <Katherine@iMac-Katherine-Swanson.local>
2023-02-15 19:22:17 +0300

    less_09

--- service/admin.py
+++ service/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 
-from .models import Product, Bin
+from .models import Product, Bin, Home
 
 # admin.site.register(Product)
 admin.site.register(Bin)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','id','cost']
+    list_display = ['title','id','cost', 'is_active']
     fields = ['id', 'title', 'cost']
     list_filter =["cost"]
     search_fields =['title']
 
 # admin.site.register(Product, ProductAdmin)
+
+@admin.register(Home)
+class HomeAdmin(admin.ModelAdmin):
+    list_display = ['size','cost','adr', 'bal']
+    # fields = ['id', 'title', 'cost']
+    # list_filter =["cost"]
+    # search_fields =['title']
