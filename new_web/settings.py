41f7a4985b6b9f98b78f3741e0b577419e91d4ca
Katherine Swanson <Katherine@iMac-Katherine-Swanson.local>
2023-02-17 11:54:17 +0300

    less_11

--- new_web/settings.py
+++ new_web/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'service.apps.ServiceConfig',
+    'web_lib.apps.WebLibConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'new_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'new_pr',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': '5432',
+        'PORT': '5432',
     }
 }
 
