a8312b8d21f60a083500e4977b0dace080298c13
Katherine Swanson <Katherine@iMac-Katherine-Swanson.local>
2023-03-02 16:42:58 +0300

    less_16

--- web_lib/views.py
+++ web_lib/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from web_lib.models import Author, Book
+
 def main(request):
     return render(request, 'web_lib/main.html')
 
 def authors(request):
     all_authors = {'authors': Author.objects.all()}
     return render(request, 'web_lib/authors.html', all_authors)
+
+def author_id(request, pk):
+    author = Author.objects.get(pk=pk)
+    books_amount = author.book_set.count()
+    found_author= {'author': author, 'books_amount': books_amount}
+    return render(request, 'web_lib/author_id.html', found_author)
 
 def books(request):
     all_books = {'books': Book.objects.all()}
