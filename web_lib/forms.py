5aace033f0a0c80a24164db01f6e0dbe571d5677
Katherine Swanson <Katherine@iMac-Katherine-Swanson.local>
2023-03-10 19:01:49 +0300

    less_20

--- web_lib/forms.py
+++ web_lib/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.forms import fields, widgets
 from .models import Book
+from django.core import validators
+from django.core.exceptions import ValidationError
 
 class SearchAuthor(forms.Form):
     author_uuid = forms.UUIDField(label="Author UUID", required=False)
@@ -12,16 +14,31 @@ class PostAuthor(forms.Form):
     email = forms.EmailField(label="Email", required=False)
 
 class BookForm(forms.ModelForm):
-
+    title = forms.CharField(
+        label="Name",
+        max_length=150,
+        required=False,
+        validators= [validators.RegexValidator(regex='^.{3,}$')],
+        error_messages={'invalid': 'Название книги слишком маленькое'},
+      ) 
     color = forms.CharField(label="Color", max_length=150, required=False)  #добавить поле
     
     class Meta:
         model = Book
         fields = '__all__'
         labels = {
-            'title': 'Название книги',
             'description':'Описание книги',
             'page_num':'Количество страниц',
             'author':'Выберите автора'
             }
         widgets = {'description': widgets.TextInput}
+
+    def clean(self):
+        super().clean()
+        errors = dict()
+        if self.cleaned_data['description'] != 'book':
+            errors['description'] = ValidationError('error desc')
+        if self.cleaned_data['page_num'] != 100:
+            errors['page_num'] = ValidationError('error page_num')
+        if errors:
+            raise ValidationError(errors)
--- web_lib/views.py
+++ web_lib/views.py
@@ -9,19 +9,6 @@ def main(request):
     return render(request, "web_lib/main.html", {"form": book_form})
 
 
-def create_book(req):
-    book_form = BookForm()
-    if req.method == "POST":
-        book_form = BookForm(req.POST)
-        if book_form.is_valid():
-            book = book_form.save(commit=False)
-            book.description = book_form.cleaned_data['description'] + ' ' + book_form.cleaned_data['color']
-            book.save()
-            book_form.save_m2m()
-            return redirect('books')
-    return render(req, "web_lib/book_form.html", {"form": book_form})
-
-
     # if request.method == "GET":
         # form = SearchAuthor(request.GET)
         # form_post = PostAuthor(request.POST)
@@ -54,6 +41,39 @@ def author_id(request, pk):
 def books(request):
     all_books = {'books': Book.objects.all()}
     return render(request, 'web_lib/books.html', all_books)
+
+def create_book(req):
+    book_form = BookForm()
+    if req.method == "POST":
+        book_form = BookForm(req.POST)
+        if book_form.is_valid():
+            book = book_form.save(commit=False)
+            book.description = book_form.cleaned_data['description'] + ' ' + book_form.cleaned_data['color']
+            book.save()
+            book_form.save_m2m()
+            return redirect('books')
+    return render(req, "web_lib/book_form.html", {"form": book_form})
+
+def update_book(req, pk):
+    book = Book.objects.get(pk=pk)
+    book_form = BookForm(instance=book)
+    if req.method == "POST":
+        book_form = BookForm(req.POST, instance=book)
+        if book_form.is_valid():
+            book = book_form.save(commit=False)
+            book.description = book_form.cleaned_data['description'] + ' ' + book_form.cleaned_data['color']
+            book.save()
+            book_form.save_m2m()
+            return redirect('books')
+    return render(req, "web_lib/book_form.html", {"form": book_form})
+
+def delete_book(req, pk):
+    book = Book.objects.get(pk=pk)
+    if req.method == "POST":
+        book.delete()
+        return redirect('books')
+    return render(req, "web_lib/delete_book.html", {'book': book})
+
 
 def about(request):
     return render(request, 'web_lib/about.html')
